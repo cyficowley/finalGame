3faf39f81cc062069b3c98b396d2bd44d0704fba
cyficowley <cyficowley@gmail.com>
2017-01-25 14:33:48 -0800

    yea

--- JavaFiles/Block.java
+++ JavaFiles/Block.java
@@ -1,4 +1,14 @@
 public class Block
 {
+	//types == {0 : air; 1 : stone;}
+	int type;
+	int xIndex;
+	int yIndex;
+	public Block( int type, int xIndex, int yIndex)
+	{
+		this.type = type;
+		this.xIndex = xIndex;
+		this.yIndex = yIndex;
+	}
 	
 }
