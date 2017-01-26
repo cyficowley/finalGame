5a0c7f70cf3fd77e0874d5416ebe4be0af950f7f
cyficowley <cyficowley@gmail.com>
2017-01-25 20:37:17 -0800

    hyep

--- JavaFiles/Block.java
+++ JavaFiles/Block.java
@@ -1,4 +1,4 @@
-public class Block
+public class Block //this is going to be the base block of the worlds, a crap ton of these will make up the world
 {
 	//types == {0 : air; 1 : stone;}
 	int type;
--- JavaFiles/Chunk.java
+++ JavaFiles/Chunk.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Chunk
+public class Chunk // this will be like a chunk in minecraft so we can generate stuff and load things more efficiently
 {
 	
 }
