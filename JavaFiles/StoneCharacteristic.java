cde97c47786c9fb6527c5fcd86e98c69f5af1c2d
cyficowley <cyficowley@gmail.com>
2017-01-26 11:30:41 -0800

    added tasks for people to do

--- JavaFiles/StoneCharacteristic.java
+++ JavaFiles/StoneCharacteristic.java
@@ -1,10 +1,14 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 public class StoneCharacteristic extends BlockCharacteristic
 {
 	Color color;
 	public StoneCharacteristic(Block block)
-	{
+	{ // import randomly one of the two images here and rotate pi/2 * (int)(MAth.random() * 4) degrees then set it to the image it will draw
 		super(block);
 		int rgb = (int)(Math.random() * 100 + 50);
 		color = new Color(rgb,rgb,rgb);
@@ -12,13 +16,13 @@ public class StoneCharacteristic extends BlockCharacteristic
 	@Override
 	public void drawMe(Graphics g)
 	{
-		g.setColor(color);
+		g.setColor(color); // make this not fill a rect but draw the image
 		g.fillRect((int)(block.xIndex * Screen.blockWidth - Screen.screenX), (int)(block.yIndex * Screen.blockWidth- Screen.screenY), (int)Screen.blockWidth, (int)Screen.blockWidth);
 	}
 	@Override
 	public void drawMe(Graphics g, Color color)
 	{
-		g.setColor(color);
+		g.setColor(color); // leave this one
 		g.fillRect((int)(block.xIndex * Screen.blockWidth - Screen.screenX), (int)(block.yIndex * Screen.blockWidth- Screen.screenY), (int)Screen.blockWidth, (int)Screen.blockWidth);
 	}
 }
