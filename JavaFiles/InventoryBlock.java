91c16b4fbd331cd5444cec3829ebf0298db60352
cyficowley <cyficowley@gmail.com>
2017-03-28 14:22:22 -0700

    made it squares instead of round square because of performance issues

--- JavaFiles/InventoryBlock.java
+++ JavaFiles/InventoryBlock.java
@@ -3,20 +3,20 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
 public class InventoryBlock{
-  public static int blockSize = 64;
-  public int xPos;
-  public int yPos;
-  public Color blockColor;
-  public static int itemID = 0;
+	public static int blockSize = 64;
+	public int xPos;
+	public int yPos;
+	public Color blockColor;
+	public static int itemID = 0;
   
   
-  public InventoryBlock(int x, int y){
-    this.xPos = x;
-    this.yPos = y;
-    blockColor = new Color(0.2f, 0.2f, 0.2f, 0.3f);
-  }
-  public void drawMe(Graphics2D g){
-    g.setColor(blockColor);
-    g.fillRoundRect(xPos, yPos, blockSize, blockSize, 5, 5);
-  }
+	public InventoryBlock(int x, int y){
+		this.xPos = x;
+		this.yPos = y;
+		blockColor = new Color(0.2f, 0.2f, 0.2f, 0.3f);
+	}
+	public void drawMe(Graphics2D g){
+		g.setColor(blockColor);
+		g.fillRect(xPos, yPos, blockSize, blockSize);
+	}
 }
--- javaFiles/Inventory.java
+++ javaFiles/Inventory.java
@@ -1,19 +1,19 @@
 package javaFiles;
 import java.awt.Graphics2D;
 public class Inventory{
-  public static InventoryBlock[][] inventory = new InventoryBlock[4][8];
-  public Inventory(){
-    for(int r = 0; r < inventory.length; r++){
-      for(int c = 0; c < inventory[r].length; c++){
-        inventory[r][c] = new InventoryBlock(c *(InventoryBlock.blockSize + 10) + 15, r *(InventoryBlock.blockSize + 10) + 15);
-      }
-    }
-  }
-  public void drawInventory(Graphics2D g){
-    for(int r = 0; r < inventory.length; r++){
-      for(int c = 0; c < inventory[r].length; c++){
-        inventory[r][c].drawMe(g);
-      }
-    }
-  }
+	public static InventoryBlock[][] inventory = new InventoryBlock[4][8];
+	public Inventory(){
+		for(int r = 0; r < inventory.length; r++){
+			for(int c = 0; c < inventory[r].length; c++){
+				inventory[r][c] = new InventoryBlock(c *(InventoryBlock.blockSize + 10) + 15, r *(InventoryBlock.blockSize + 10) + 15);
+			}
+		}
+	}
+	public void drawInventory(Graphics2D g){
+		for(int r = 0; r < inventory.length; r++){
+			for(int c = 0; c < inventory[r].length; c++){
+				inventory[r][c].drawMe(g);
+			}
+		}
+	}
 }
