9e4633da28650f3accd1345f8cbb64a609af8609
cyficowley <cyficowley@gmail.com>
2017-03-28 22:06:00 -0700

    small little bitty update

--- JavaFiles/InventoryBlock.java
+++ JavaFiles/InventoryBlock.java
@@ -7,13 +7,21 @@ public class InventoryBlock{
 	public int xPos;
 	public int yPos;
 	public Color blockColor;
-	public static int itemID = 0;
-  
-  
+	public InventoryObject inventoryObject;
+	boolean empty = false;
+
 	public InventoryBlock(int x, int y){
 		this.xPos = x;
 		this.yPos = y;
 		blockColor = new Color(0.2f, 0.2f, 0.2f, 0.3f);
+		empty = true;
+	}
+
+	public InventoryBlock(int x, int y, InventoryObject inventoryObject){
+		this.xPos = x;
+		this.yPos = y;
+		blockColor = new Color(0.2f, 0.2f, 0.2f, 0.3f);
+		this.inventoryObject = inventoryObject;
 	}
 	public void drawMe(Graphics2D g){
 		g.setColor(blockColor);
---
+++ javaFiles/InventoryObject.java
@@ -0,0 +1,18 @@
+package javaFiles;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+public class InventoryObject
+{
+	public InventoryObject()
+	{}
+
+	public void actionLeft()
+	{}
+
+	public void actionRight()
+	{}
+
+	public void onInInventoryClick()
+	{}
+}
